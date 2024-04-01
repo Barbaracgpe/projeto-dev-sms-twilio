dad07e7227f4eb9de3ebe55e7332a295347a916c
Perkles <barbara.babi14@hotmail.com>
2024-04-01 20:28:32 -0300

    Initial commit

---
+++ src/main/java/me/bonfim/projetodevsmstwilio/ProjetoDevSmsTwilioApplication.java
@@ -0,0 +1,13 @@
+package me.bonfim.projetodevsmstwilio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoDevSmsTwilioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoDevSmsTwilioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/bonfim/projetodevsmstwilio/ProjetoDevSmsTwilioApplicationTests.java
@@ -0,0 +1,13 @@
+package me.bonfim.projetodevsmstwilio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoDevSmsTwilioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
